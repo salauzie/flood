5b42c98ed358ddcf599dd8d56dbde8690825c56b
Stephanie Lauziere <salauzie@gmail.com>
2015-03-13 19:35:23 -0400

    incorporated google maps api with weather, traffic, and geolocation

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,35 @@
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 9,
+//     center: new google.maps.LatLng(-25.363882, 131.044922)
+//   };
+
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+
+//   var marker = new google.maps.Marker({
+//     position: map.getCenter(),
+//     icon: {
+//       path: google.maps.SymbolPath.CIRCLE,
+//       scale: 10
+//     },
+//     draggable: true,
+//     map: map
+//   });
+
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+// var mapOptions = {
+//     center: new google.maps.LatLng(37.7831, -122.4039),
+//     zoom: 12,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+// };
+// var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+// var markerOptions = {
+//     position: new google.maps.LatLng(37.7831, -122.4039)
+// };
+// var marker = new google.maps.Marker(markerOptions);
+// marker.setMap(map);
